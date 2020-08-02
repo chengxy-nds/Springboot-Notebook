d7a89a6ec5a3347b5221014bf225f1b5aeec3a6f
xin <515361725@qq.com>
2020-08-02 22:37:55 +0800

    springboot-database-doc

---
+++ springboot-database-doc/src/test/java/com/xiaofu/doc/SpringbootDatabaseDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaofu.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDatabaseDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
