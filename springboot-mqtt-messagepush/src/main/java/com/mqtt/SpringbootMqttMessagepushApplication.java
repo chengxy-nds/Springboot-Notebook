0b93f6cfd108d3108cc9057d32774c8aaa7ae5aa
xin <515361725@qq.com>
2020-09-07 18:31:46 +0800

    springboot-mqtt-messagepush

---
+++ springboot-mqtt-messagepush/src/main/java/com/mqtt/SpringbootMqttMessagepushApplication.java
@@ -0,0 +1,13 @@
+package com.mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMqttMessagepushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMqttMessagepushApplication.class, args);
+	}
+
+}
