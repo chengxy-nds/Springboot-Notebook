6e3909011672413c47b4bbae394f7c1edd88f085
xin <515361725@qq.com>
2020-11-04 18:01:22 +0800

    图片识别

---
+++ springboot-identify-img/src/main/java/com/xiaofu/SpringbootIdentifyImgApplication.java
@@ -0,0 +1,13 @@
+package com.xiaofu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIdentifyImgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIdentifyImgApplication.class, args);
+	}
+
+}
