02b74c325811691710cfb087fa647ce3f597da39
xin <515361725@qq.com>
2020-11-04 17:57:15 +0800

    redis + lua 限流器

---
+++ springboot-aop-redis-lua/src/main/java/com/xiaofu/limit/impl/RedisLimiterHelper.java
@@ -0,0 +1,23 @@
+package com.xiaofu.limit.impl;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+@Configuration
+public class RedisLimiterHelper {
+
+    @Bean
+    public RedisTemplate<String, Serializable> limitRedisTemplate(LettuceConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Serializable> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
