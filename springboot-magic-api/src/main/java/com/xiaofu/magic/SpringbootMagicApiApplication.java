d665d440d1c601718e6ac2689c0679f335babeaf
zhifu.xin <zhifu.xin@ipinyou.com>
2021-04-04 20:44:44 +0800

    magic-api 快速创建API接口

---
+++ springboot-magic-api/src/main/java/com/xiaofu/magic/SpringbootMagicApiApplication.java
@@ -0,0 +1,13 @@
+package com.xiaofu.magic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMagicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMagicApiApplication.class, args);
+	}
+
+}
---
+++ springboot-magic-api/src/test/java/com/xiaofu/magic/SpringbootMagicApiApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.xiaofu.magic;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringbootMagicApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
