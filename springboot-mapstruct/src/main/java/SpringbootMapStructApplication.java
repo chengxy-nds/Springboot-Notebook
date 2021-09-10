2cc57cea61e34d4714b1a9de09dee2811e9f034c
zhifu.xin <zhifu.xin@ipinyou.com>
2021-09-10 17:23:18 +0800

    MapStruct

---
+++ springboot-mapstruct/src/main/java/SpringbootMapStructApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMapStructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMapStructApplication.class, args);
+	}
+
+}
---
+++ springboot-mapstruct/src/test/java/com/xiaofu/mapstruct/MapStruct_1Test.java
@@ -0,0 +1,13 @@
+package com.xiaofu.mapstruct;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * @Auther: zhifu.xin@ipinyou.com
+ * @Date: 2021/9/10 15:56
+ * @Description:
+ */
+public class MapStruct_1Test extends TestCase {
+
+}
